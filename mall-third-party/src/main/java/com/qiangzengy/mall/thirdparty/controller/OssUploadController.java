16f93697dae43f3697f994ae0339052e19070904
qiangzengy <qiangzengy@163.com>
2020-04-26 00:21:40 +0800

    file upload

--- mall-third-party/src/main/java/com/qiangzengy/mall/thirdparty/controller/OssUploadController.java
+++ mall-third-party/src/main/java/com/qiangzengy/mall/thirdparty/controller/OssUploadController.java
@@ -4,17 +4,22 @@ package com.qiangzengy.mall.thirdparty.controller;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.GeneratePresignedUrlRequest;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import com.qiangzengy.common.utils.R;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/third/party")
@@ -72,6 +77,50 @@ public class OssUploadController {
         return R.ok().put("data",respMap);
     }
 
+
+
+    public R uploadFile(MultipartFile file) {
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessId, accessKey);
+
+        try {
+            // 创建OSS实例。
+            //获取上传文件输入流
+            InputStream inputStream = file.getInputStream();
+            //获取文件名称
+            String fileName = file.getOriginalFilename();
+
+            //1 在文件名称里面添加随机唯一的值
+            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            // yuy76t5rew01.jpg
+            fileName = uuid + fileName;
+
+            //2 把文件按照日期进行分类
+            //获取当前日期
+            String datePath = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            //拼接
+            fileName = datePath + "/" + fileName;
+
+            //调用oss方法实现上传
+            //第一个参数  Bucket名称
+            //第二个参数  上传到oss文件路径和文件名称
+            //第三个参数  上传文件输入流
+            ossClient.putObject(bucket, fileName, inputStream);
+
+            //加签名处理
+            // 指定过期时间为1年。
+            Date expiration = new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 365 );
+            GeneratePresignedUrlRequest req = new GeneratePresignedUrlRequest(bucket, fileName);
+            req.setExpiration(expiration);
+            URL signedUrl = ossClient.generatePresignedUrl(req);
+            return R.ok().put("data",String.valueOf(signedUrl));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }finally {
+            // 关闭OSSClient。
+            ossClient.shutdown();
+        }
+    }
 
 
 
