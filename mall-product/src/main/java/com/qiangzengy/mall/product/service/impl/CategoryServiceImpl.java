d0ca7c527ea5ad779d96208fe238a2dc0a86be02
qiangzengy <qiangzengy@163.com>
2020-05-14 21:12:20 +0800

    SpringCache整合

--- mall-product/src/main/java/com/qiangzengy/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/qiangzengy/mall/product/service/impl/CategoryServiceImpl.java
@@ -11,7 +11,6 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
