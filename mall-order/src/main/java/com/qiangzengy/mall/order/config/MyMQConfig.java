445f1b46b8e8157d5a2b8a9c445ae6fa69b95934
qiangzeng <qiangzengy@163.com>
2020-10-29 16:49:45 +0800

    优化

--- mall-order/src/main/java/com/qiangzengy/mall/order/config/MyMQConfig.java
+++ mall-order/src/main/java/com/qiangzengy/mall/order/config/MyMQConfig.java
@@ -27,17 +27,16 @@ public class MyMQConfig {
 
     @RabbitListener(queues = "order.release.order.queue")
     public void listenerQueue(OrderEntity orderEntity, Channel channel, Message message) throws IOException {
-        System.out.println("监听成功。。。。。。。。;订单ID："+orderEntity.getOrderSn());
 
-        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        System.out.println("监听成功。。。。。。。。;订单ID：" + orderEntity.getOrderSn());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 
     }
 
 
-
     //bean Queue\Binding\Exchange
     @Bean
-    public Queue orderDelayQueue(){
+    public Queue orderDelayQueue() {
 
         /**
          * new Queue(String name, boolean durable, boolean exclusive, boolean autoDelete,
@@ -49,56 +48,53 @@ public class MyMQConfig {
          * x x-dead-letter-routing-key: order.reLease.order
          * x-message-ttl: 60000
          */
-        Map<String, Object> arguments=new HashMap<>();
-        arguments.put("x-dead-letter-exchange","order-event-exchange");
-        arguments.put("x-dead-letter-routing-key","order.release.order");
-        arguments.put("x-message-ttl",60000);
-
-        Queue queue=new Queue("order.delay.queue",true,false,false,arguments);
-        return queue;
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-dead-letter-exchange", "order-event-exchange");
+        arguments.put("x-dead-letter-routing-key", "order.release.order");
+        arguments.put("x-message-ttl", 60000);
+        return new Queue("order.delay.queue", true, false, false, arguments);
 
     }
 
     @Bean
-    public Queue orderReleaseOrderQueue(){
-
-        Queue queue=new Queue("order.release.order.queue",true,false,false);
-        return queue;
+    public Queue orderReleaseOrderQueue() {
+        return new Queue("order.release.order.queue", true, false, false);
     }
 
     @Bean
-    public Binding orderCreateOrderBinding(){
-
-        Binding binding = new Binding("order.delay.queue", Binding.DestinationType.QUEUE,
-                "order-event-exchange", "order.create.order", null);
-        return binding;
+    public Binding orderCreateOrderBinding() {
+        return new Binding("order.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.create.order",
+                null);
     }
 
     @Bean
-    public Binding orderReleaseOrderBinding(){
-
-        Binding binding = new Binding("order.release.order.queue", Binding.DestinationType.QUEUE,
-                "order-event-exchange", "order.release.order", null);
-        return binding;
+    public Binding orderReleaseOrderBinding() {
+        return new Binding("order.release.order.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.order",
+                null);
 
     }
 
     @Bean
-    public Exchange orderEventExchange(){
-
-       // String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
-        TopicExchange topicExchange = new TopicExchange("order-event-exchange", true, false);
-        return topicExchange;
+    public Exchange orderEventExchange() {
+        // String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        return new TopicExchange("order-event-exchange", true, false);
 
     }
 
 
     @Bean
-    public Binding orderReleaseOtherBinding(){
-
-        Binding binding = new Binding("stock.release.stock.queue", Binding.DestinationType.QUEUE,
-                "order-event-exchange", "order.release.other.#", null);
-        return binding;
+    public Binding orderReleaseOtherBinding() {
+        return new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.other.#",
+                null);
 
     }
 }
