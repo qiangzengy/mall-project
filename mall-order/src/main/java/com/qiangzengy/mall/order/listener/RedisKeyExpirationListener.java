8f401819a47acc4568621fddfe65f129ae071806
qiangzeng <qiangzengy@163.com>
2020-11-10 11:55:09 +0800

    redis 实现延时队列

--- mall-order/src/main/java/com/qiangzengy/mall/order/listener/RedisKeyExpirationListener.java
+++ mall-order/src/main/java/com/qiangzengy/mall/order/listener/RedisKeyExpirationListener.java
@@ -1,12 +1,15 @@
 package com.qiangzengy.mall.order.listener;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 /**
+ *
+ * redis实现延时队列
+ * 参考：https://mp.weixin.qq.com/s/enmWh5TUD9_2J82M1ZJLew
+ *
  * @author qiangzengy@gmail.com
  * @date 2020/11/10
  *
@@ -16,7 +19,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
 
-    public RedisKeyExpirationListener(@Qualifier("container") RedisMessageListenerContainer listenerContainer) {
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
         super(listenerContainer);
     }
 
@@ -29,6 +32,6 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
     public void onMessage(Message message, byte[] pattern) {
         // 获取到失效的 key，进行取消订单业务处理
         String expiredKey = message.toString();
-        System.out.println(expiredKey);
+        System.out.println("============expiredKey："+expiredKey);
     }
 }
